28c76714c7a5897867b9bd96b2c7c7069e2ce0a1
Max <maxcullifer@gmail.com>
2024-12-12 17:35:34 -0600

    fix: optimize
    recreated visited with spread syntax is a performance destroyer. So just pass in the main visited to BFS then return a new set that is the region.

--- src/day12/day12.ts
+++ src/day12/day12.ts
@@ -17,21 +17,22 @@ function inBounds(point: Point, maxRow: number, maxCol: number) {
 	return point[0] >= 0 && point[0] < maxRow && point[1] >= 0 && point[1] < maxCol;
 }
 
-function BFS(input: string[], start: Point = [0, 0]): [Set<string>, number] {
-	let visited = new Set<string>();
+function BFS(visited: Set<string>, input: string[], start: Point = [0, 0]): [Set<string>, number] {
+	let region = new Set<string>();
 	let queue: Point[] = [start];
 	let current: Point | undefined;
 	let cornerCount = 0;
 	while ((current = queue.shift())) {
-		if (visited.has(current.toString())) continue;
+		if (region.has(current.toString())) continue;
 		visited.add(current.toString());
+		region.add(current.toString());
 		const [row, col] = current;
 		let cornersAdded = 4;
 		for (let direction of directions) {
 			let next: Point = [row + direction[0], col + direction[1]];
 			if (
 				inBounds(next, input.length, input[0].length) &&
-				!visited.has(next.toString()) &&
+				!region.has(next.toString()) &&
 				input[next[0]][next[1]] === input[row][col]
 			) {
 				queue.push(next);
@@ -40,7 +41,7 @@ function BFS(input: string[], start: Point = [0, 0]): [Set<string>, number] {
 		}
 		cornerCount += cornersAdded;
 	}
-	return [visited, cornerCount];
+	return [region, cornerCount];
 }
 
 function part1(input: string[]) {
@@ -49,9 +50,8 @@ function part1(input: string[]) {
 	for (let i = 0; i < input.length; i++) {
 		for (let j = 0; j < input.length; j++) {
 			if (visited.has([i, j].toString())) continue;
-			let [region, cornerCount] = BFS(input, [i, j]);
+			let [region, cornerCount] = BFS(visited, input, [i, j]);
 			cost += cornerCount * region.size;
-			visited = new Set([...visited, ...region]);
 		}
 	}
 	return cost;
@@ -63,10 +63,9 @@ function part2(input: string[]) {
 	for (let i = 0; i < input.length; i++) {
 		for (let j = 0; j < input.length; j++) {
 			if (visited.has([i, j].toString())) continue;
-			let [region, cornerCount] = BFS(input, [i, j]);
+			let [region, cornerCount] = BFS(visited, input, [i, j]);
 			let adjacentCount = 0;
 			cost += adjacentCount * region.size;
-			visited = new Set([...visited, ...region]);
 		}
 	}
 	return cost;
