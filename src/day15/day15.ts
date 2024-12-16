30fd8fc149da87d0cbdbc296599e88bf6c2c84f5
Max <maxcullifer@gmail.com>
2024-12-15 22:50:27 -0600

    refactor: some more cleanup

--- src/day15/day15.ts
+++ src/day15/day15.ts
@@ -4,59 +4,61 @@ import { Reader } from '@/util/reader';
 type Point = [number, number];
 type Direction = '^' | 'v' | '<' | '>';
 class Box {
-	left: Point;
-	right: Point;
+	id: Symbol;
+	row: number;
+	col: number;
+	size: number;
 	leftChar: string;
 	rightChar: string;
-	constructor(left: Point, right?: Point) {
-		if (right !== undefined) {
-			this.left = left;
-			this.right = right;
+	constructor(pos: Point, size = 1) {
+		this.id = Symbol();
+		this.size = size;
+		this.row = pos[0];
+		this.col = pos[1];
+		if (size === 1) {
+			this.leftChar = this.rightChar = 'O';
+		} else {
 			this.leftChar = '[';
 			this.rightChar = ']';
-		} else {
-			this.left = this.right = left;
-			this.leftChar = this.rightChar = 'O';
 		}
 	}
 
 	get gpsCoordinate() {
-		return 100 * this.left[0] + this.left[1];
+		return 100 * this.row + this.col;
 	}
 
 	move(direction: Direction) {
-		this.left = addDirection(this.left, direction);
-		this.right = addDirection(this.right, direction);
+		[this.row, this.col] = addDirection([this.row, this.col], direction);
+		this.id = Symbol();
 	}
 
 	canMove(input: Array<string[]>, direction: Direction) {
-		let nextLeft = addDirection(this.left, direction);
-		let nextRight = addDirection(this.right, direction);
-		if (input[nextLeft[0]][nextLeft[1]] === '#' || input[nextRight[0]][nextRight[1]] === '#') {
-			return false;
-		}
-		return true;
+		let [nextRow, nextCol] = addDirection([this.row, this.col], direction);
+		const headHitsWall = input[nextRow][nextCol] === '#';
+		const tailHitsWall = input[nextRow][nextCol + this.size - 1] === '#';
+		return !(headHitsWall || tailHitsWall);
 	}
 
 	touchesHorizontally(box: Box, direction: Direction) {
-		const isInSameRow = box.left[0] === this.left[0] && box.right[0] === this.right[0];
 		const dir = direction === '<' ? -1 : 1;
-		const isNextTo = this.left[1] + dir === box.right[1] || this.right[1] + dir === box.left[1];
-		return isInSameRow && isNextTo;
+		const matchesVertical = this.row === box.row;
+		const touchesTail = this.col + dir === box.col + box.size - 1;
+		const touchesHead = this.col + dir + this.size - 1 === box.col;
+		return matchesVertical && (touchesTail || touchesHead);
 	}
 
 	touchesVertically(box: Box, direction: Direction) {
 		let dir = direction === '^' ? -1 : 1;
-		let matchesLeftVertically =
-			this.left[0] + dir === box.left[0] || this.left[0] + dir === box.right[0];
-		let matchesRightVertically =
-			this.right[0] + dir === box.left[0] || this.right[0] + dir === box.right[0];
-		let touchesLeftHorizontally = this.left[1] === box.left[1] || this.left[1] === box.right[1];
-		let touchesRightHorizontally = this.right[1] === box.left[1] || this.right[1] === box.right[1];
-		return (
-			(matchesLeftVertically || matchesRightVertically) &&
-			(touchesLeftHorizontally || touchesRightHorizontally)
-		);
+		const matchesVertical = this.row + dir === box.row;
+		const touchesTail = this.col <= box.col && this.col + this.size - 1 >= box.col;
+		const touchesHead = this.col >= box.col && this.col <= box.col + box.size - 1;
+		return matchesVertical && (touchesTail || touchesHead);
+	}
+
+	touches(box: Box, direction: Direction) {
+		return direction === '^' || direction === 'v'
+			? this.touchesVertically(box, direction)
+			: this.touchesHorizontally(box, direction);
 	}
 }
 
@@ -67,23 +69,24 @@ function setup(input: string[], double = false): [Array<string[]>, Direction[], 
 	let onMovements = false;
 	let boxes: Box[] = [];
 	for (let i = 0; i < input.length; i++) {
-		if (input[i].length === 0) {
-			onMovements = true;
-		}
 		if (onMovements) {
 			movements.push(...(input[i].split('') as Direction[]));
 		} else {
+			if (input[i].length === 0) {
+				onMovements = true;
+				continue;
+			}
 			let toAdd = [];
 			for (let j = 0; j < input[i].length; j++) {
 				let char = input[i][j];
 				if (char === 'O') {
 					if (double) {
-						boxes.push(new Box([i, toAdd.length], [i, toAdd.length + 1]));
+						boxes.push(new Box([i, toAdd.length], 2));
 						toAdd.push('[');
 						toAdd.push(']');
 					} else {
 						toAdd.push('O');
-						boxes.push(new Box([i, j]));
+						boxes.push(new Box([i, toAdd.length - 1]));
 					}
 				} else if (char === '@') {
 					if (double) {
@@ -92,7 +95,7 @@ function setup(input: string[], double = false): [Array<string[]>, Direction[], 
 						toAdd.push('.');
 					} else {
 						toAdd.push('@');
-						start = [i, j];
+						start = [i, toAdd.length - 1];
 					}
 				} else {
 					toAdd.push(char);
@@ -120,29 +123,22 @@ function addDirection(pos: Point, dir: 'v' | '^' | '<' | '>'): Point {
 	}
 }
 
-function DFS(box: Box, boxes: Box[], direction: Direction) {
+function BFS(box: Box, boxes: Box[], direction: Direction, map: Array<string[]>) {
 	let queue: Box[] = [box];
 	let current: Box | undefined;
 	let touchingBoxes = [];
-	let visited = new Set<string>();
-	const vertical = direction === '^' || direction === 'v';
 	while (queue.length > 0) {
-		current = queue.pop();
+		current = queue.shift();
 		if (!current) break;
-		visited.add(current.left.toString());
+		if (!current.canMove(map, direction)) {
+			touchingBoxes = [];
+			break;
+		}
 		touchingBoxes.push(current);
 		for (let i = 0; i < boxes.length; i++) {
-			if (
-				vertical &&
-				current.touchesVertically(boxes[i], direction) &&
-				!visited.has(boxes[i].left.toString())
-			) {
-				queue.push(boxes[i]);
-			} else if (
-				!vertical &&
-				current.touchesHorizontally(boxes[i], direction) &&
-				!visited.has(boxes[i].left.toString())
-			) {
+			if (current.id === boxes[i].id) continue;
+			let touches = current.touches(boxes[i], direction);
+			if (touches) {
 				queue.push(boxes[i]);
 			}
 		}
@@ -150,41 +146,33 @@ function DFS(box: Box, boxes: Box[], direction: Direction) {
 	return touchingBoxes;
 }
 
-function push2(map: Array<string[]>, current: Point, direction: Direction, boxes: Box[]): Point {
+function push(map: Array<string[]>, current: Point, direction: Direction, boxes: Box[]): Point {
 	let next = addDirection(current, direction);
 	let nextChar = map[next[0]][next[1]];
 	if (nextChar === '#') {
 		return current;
-	}
-	if (nextChar === '.') {
+	} else if (nextChar === '.') {
 		map[next[0]][next[1]] = '@';
 		map[current[0]][current[1]] = '.';
 		return next;
 	}
 	let box = boxes.find(
 		(box) =>
-			(box.left[0] === next[0] && box.left[1] === next[1]) ||
-			(box.right[0] === next[0] && box.right[1] === next[1])
+			(box.row === next[0] && box.col === next[1]) ||
+			(box.row === next[0] && box.col + box.size - 1 === next[1])
 	);
-	if (!box) {
-		return next;
-	}
-	let touchingBoxes = DFS(box, boxes, direction);
-	let canMove = 0;
-	for (let i = 0; i < touchingBoxes.length; i++) {
-		if (touchingBoxes[i].canMove(map, direction)) {
-			canMove++;
-		}
-	}
-	if (canMove !== touchingBoxes.length) {
+	if (!box) return next;
+	let touchingBoxes = BFS(box, boxes, direction, map);
+	if (touchingBoxes.length === 0) {
 		return current;
 	}
 	for (let i = touchingBoxes.length - 1; i >= 0; i--) {
-		map[touchingBoxes[i].left[0]][touchingBoxes[i].left[1]] = '.';
-		map[touchingBoxes[i].right[0]][touchingBoxes[i].right[1]] = '.';
+		map[touchingBoxes[i].row][touchingBoxes[i].col] = '.';
+		map[touchingBoxes[i].row][touchingBoxes[i].col + touchingBoxes[i].size - 1] = '.';
 		touchingBoxes[i].move(direction);
-		map[touchingBoxes[i].left[0]][touchingBoxes[i].left[1]] = touchingBoxes[i].leftChar;
-		map[touchingBoxes[i].right[0]][touchingBoxes[i].right[1]] = touchingBoxes[i].rightChar;
+		map[touchingBoxes[i].row][touchingBoxes[i].col] = touchingBoxes[i].leftChar;
+		map[touchingBoxes[i].row][touchingBoxes[i].col + touchingBoxes[i].size - 1] =
+			touchingBoxes[i].rightChar;
 	}
 	map[next[0]][next[1]] = '@';
 	map[current[0]][current[1]] = '.';
@@ -193,9 +181,8 @@ function push2(map: Array<string[]>, current: Point, direction: Direction, boxes
 
 function part1(input: string[]) {
 	let [map, movements, start, boxes] = setup(input);
-	let pos = start;
 	for (let movement of movements) {
-		pos = push2(map, pos, movement, boxes);
+		start = push(map, start, movement, boxes);
 	}
 	let sum = 0;
 	for (let box of boxes) {
@@ -206,15 +193,13 @@ function part1(input: string[]) {
 
 function part2(input: string[]) {
 	let [map, movements, start, boxes] = setup(input, true);
-	let pos = start;
 	for (let movement of movements) {
-		pos = push2(map, pos, movement, boxes);
+		start = push(map, start, movement, boxes);
 	}
 	let sum = 0;
 	for (let box of boxes) {
 		sum += box.gpsCoordinate;
 	}
-
 	return sum;
 }
 
