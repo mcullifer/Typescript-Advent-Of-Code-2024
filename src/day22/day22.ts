dc84bfae71a92d5b1485b348e9e2110e3ed1fb56
Max <maxcullifer@gmail.com>
2024-12-22 15:47:52 -0600

    chore: fix some variable names

--- src/day22/day22.ts
+++ src/day22/day22.ts
@@ -40,37 +40,37 @@ function part1(input: string[]) {
 function part2(input: string[]) {
 	const secretNumbers = input.map((line) => parseInt(line));
 	const numsPerDay = 2000;
-	let sequences = [];
+	let buyerPrices = [];
 	for (let secretNumber of secretNumbers) {
-		let seq = [secretNumber];
+		let tempPrices = [secretNumber];
 		for (let i = 0; i < numsPerDay; i++) {
 			secretNumber = getNextSecretNumber(secretNumber);
-			seq.push(secretNumber);
+			tempPrices.push(secretNumber);
 		}
-		sequences.push(seq);
+		buyerPrices.push(tempPrices);
 	}
 	let sequenceSums = new Map<string, number>();
 	let seen = new Set<string>();
 	let maxSum = 0;
 	let maxSequence = '';
-	for (let sequence of sequences) {
+	for (let prices of buyerPrices) {
 		seen.clear();
-		for (let j = 1; j < sequence.length - 3; j++) {
+		for (let j = 1; j < prices.length - 3; j++) {
 			let seq = [];
-			let lastOnesPlace = 0;
+			let sequenceValue = 0;
 			for (let k = 0; k < 4; k++) {
-				let prevOnesPlace = sequence[j + k - 1] % 10;
-				let currentOnesPlace = sequence[j + k] % 10;
-				let diff = currentOnesPlace - prevOnesPlace;
+				let prevLastDigit = prices[j + k - 1] % 10;
+				let currentLastDigit = prices[j + k] % 10;
+				let diff = currentLastDigit - prevLastDigit;
 				if (k === 3) {
-					lastOnesPlace = currentOnesPlace;
+					sequenceValue = currentLastDigit;
 				}
 				seq.push(diff);
 			}
 			let sequenceId = `${seq[0]},${seq[1]},${seq[2]},${seq[3]}`;
 			if (seen.has(sequenceId)) continue;
 			seen.add(sequenceId);
-			let next = (sequenceSums.get(sequenceId) ?? 0) + lastOnesPlace;
+			let next = (sequenceSums.get(sequenceId) ?? 0) + sequenceValue;
 			sequenceSums.set(sequenceId, next);
 			if (next > maxSum) {
 				maxSum = next;
