0e0bc93b0b9706fe00098294fa3fc337a9b66485
Max <maxcullifer@gmail.com>
2024-12-22 10:54:35 -0600

    WIP: day 22 pt2

--- src/day22/day22.ts
+++ src/day22/day22.ts
@@ -40,8 +40,8 @@ function part1(input: string[]) {
 function part2(input: string[]) {
 	const secretNumbers = input.map((line) => parseInt(line));
 	const numsPerDay = 2000;
-	let sum = 0;
 	let diffsPerSecretNumber = [];
+	let ones = [];
 	for (let secretNumber of secretNumbers) {
 		let sequence = [secretNumber];
 		let onesPlace = [];
@@ -56,6 +56,7 @@ function part2(input: string[]) {
 			diffs.push(diff);
 		}
 		diffsPerSecretNumber.push(diffs);
+		ones.push(onesPlace);
 	}
 	// We actually want to find inside the diffs array repeating sequences
 	// that are 4 long and check if that sequence appears for the others
@@ -63,11 +64,43 @@ function part2(input: string[]) {
 	// but we do want the sequence that produces the largest number
 	// the number is the index of when this diff sequence occurs
 	// so might need to bring out the sequence arrays too
-	console.log(diffsPerSecretNumber);
-	return sum;
+	let offset = 4;
+	let sequenceMap: Record<string, number[]> = {};
+	let sequenceId = '';
+	let sequence2Id = '';
+	let maxSum = 0;
+	let maxSequence = '';
+	let tempSum = 0;
+	for (let i = 0; i < diffsPerSecretNumber[0].length - offset - 1; i++) {
+		sequenceId = diffsPerSecretNumber[0].slice(i, i + offset).join(',');
+		let nextNumber = ones[0][i + offset];
+		for (let j = 1; j < secretNumbers.length; j++) {
+			for (let k = 0; k < diffsPerSecretNumber[j].length - offset - 1; k++) {
+				sequence2Id = diffsPerSecretNumber[j].slice(k, k + offset).join(',');
+				if (sequenceId !== sequence2Id) continue;
+				let nextNumber2 = ones[j][k + offset];
+				if (!sequenceMap[sequenceId]) {
+					sequenceMap[sequenceId] = Array(secretNumbers.length).fill(0);
+					sequenceMap[sequenceId][0] = nextNumber;
+				}
+				sequenceMap[sequenceId][j] = nextNumber2;
+				tempSum = 0;
+				for (let num of sequenceMap[sequenceId]) {
+					tempSum += num;
+				}
+				if (tempSum > maxSum) {
+					maxSum = tempSum;
+					maxSequence = sequenceId;
+				}
+			}
+		}
+	}
+
+	console.log(maxSequence, maxSum);
+	return maxSum;
 }
 
 const test = Reader.read(22, 'test');
 const input = Reader.read(22, 'input');
 Benchmark.run(part1, test);
-Benchmark.run(part2, test);
+Benchmark.run(part2, input);
